1d60783adff089449ff27d3e013028bd630dc0e8
Tony Champion <tonyc@championdatasolutions.com>
2019-09-15 08:15:57 -0500

    Updated return object

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ const app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
 
+  let ret = {
+      url : req.query.url
+  };
+
   request({uri: req.query.url}, function(error, response, body){
+    ret.error = error;
+    ret.responseCode = response.statusCode;
+
     if(!error && response.statusCode == 200){
         const {JSDOM} = jsdom;
         const dom = new JSDOM(body);
 
-        var userId = dom.window.document.getElementById('user-id').value;
-        res.send(userId);
-    } else {
-        res.send(response.statusCode);
-    }
+        const userId = dom.window.document.getElementById('user-id').value;
+        ret.userId = userId;
+    } 
+
+    res.send(JSON.stringify(ret));
 });
 
 })
